87d612a036d49b54268d24ec24aa5ccaa26109fe
Rulo0 <raul@muapp.me>
2017-05-28 23:29:42 -0500

    Profile view

--- app/src/main/java/me/muapp/android/UI/Fragment/MatchingUserProfileFragment.java
+++ app/src/main/java/me/muapp/android/UI/Fragment/MatchingUserProfileFragment.java
@@ -91,6 +91,8 @@ public class MatchingUserProfileFragment extends Fragment implements ChildEventL
         adapter = new MatchingUserContentAdapter(getContext(), matchingUser);
         adapter.setShowMenuButton(false);
         adapter.setFragmentManager(getChildFragmentManager());
+
+
         adapter.setOnProfileScrollListener(onProfileScrollListener);
         userReference = FirebaseDatabase.getInstance().getReference().child(DATABASE_REFERENCE).child("content").child(String.valueOf(matchingUser.getId()));
         FirebaseDatabase.getInstance().getReference().child(DATABASE_REFERENCE).child("quotes").addListenerForSingleValueEvent(new ValueEventListener() {
