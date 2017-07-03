083924da35623f85b5760d687bb2fcf704ef3f7f
Rulo0 <raul@muapp.me>
2017-07-03 11:45:21 -0500

    BUGS

--- app/src/main/java/me/muapp/android/Classes/FirebaseMessaging/MuappInstanceIDService.java
+++ app/src/main/java/me/muapp/android/Classes/FirebaseMessaging/MuappInstanceIDService.java
@@ -23,7 +23,7 @@ public class MuappInstanceIDService extends FirebaseInstanceIdService {
     @Override
     public void onTokenRefresh() {
         String refreshedToken = FirebaseInstanceId.getInstance().getToken();
-        Log.wtf(TAG, "Refreshed token: " + refreshedToken);
+        Log.wtf(TAG, "Refreshed token:" + refreshedToken);
         sendRegistrationToServer(refreshedToken);
     }
 
